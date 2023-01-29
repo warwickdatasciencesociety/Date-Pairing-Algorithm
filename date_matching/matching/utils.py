5d03479d2eccaf16b2fc5d05e3a097f511b61258
Abhyuday <abhyuday10@gmail.com>
2023-01-29 20:30:16 +0000

    added utils

---
+++ date_matching/matching/utils.py
@@ -0,0 +1,14 @@
+import os
+import math
+
+# def print_terminal_line():
+#     t_size = os.get_terminal_size().columns-1
+#     print('='*t_size)
+
+
+def print_terminal_line(title=''):
+    title = title.upper()
+    t_size = os.get_terminal_size().columns-1
+    string_length = len(title)
+    padding = (t_size - string_length) // 2
+    print('='*padding + title + '='*padding)
